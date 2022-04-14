b966d753882cda0d48e7657bd76a4c1c9a6a6873
CamielVilla <camielvilla@gmail.com>
2022-04-14 17:06:24 +0200

    huiswerk afgemaakt zonder bonusopdrachten

--- dist/index.8f215b5e.js
+++ dist/index.8f215b5e.js
@@ -1,3 +1,11 @@
-console.log('Hallo daar!');
+async function giveCountry() {
+    try {
+        const result = await axios.get('https://restcountries.com/v2/all');
+        console.log(result);
+    } catch (e) {
+        console.error(e);
+    }
+}
+giveCountry();
 
 //# sourceMappingURL=index.8f215b5e.js.map
--- src/app.js
+++ src/app.js
@@ -1,1 +1,49 @@
-console.log('Hallo daar!');
+import axios from "axios";
+
+export function giveRegionColor(region) {
+    if (region == "Europe"){
+        return "Europe";
+    }else if(region == "Africa") {
+        return "Africa";
+    } else if (region == "Americas"){
+        return "Americas"
+    }else if (region == "Asia"){
+        return "Asia";
+    } else if(region == "Oceania"){
+        return "Oceania";
+        }else {
+        return "Other";
+    }
+
+}
+async function giveCountry() {
+    try{
+        const result = await
+            axios.get('https://restcountries.com/v2/all');
+            // console.log(result);
+            const sorted = result.data.sort((a, b) => a.population - b.population);
+            // console.log(sorted);
+        for (let i = 0; i < sorted.length ; i++) {
+            const countryName = document.createElement('li');
+            countryName.className = giveRegionColor(sorted[i].region);
+            console.log(countryName.className);
+            const countryFlag = document.createElement('img');
+            const countryPopulation = document.createElement('p');
+            countryName.textContent = sorted[i].name;
+            // countryName.style.color = color;
+            countryFlag.src = sorted[i].flag;
+            countryFlag.width="50";
+            countryPopulation.textContent = "has a population of " + sorted[i].population + " people";
+            const countryList = document.getElementById("country-name");
+            countryList.appendChild(countryName);
+            countryList.appendChild(countryFlag);
+            countryList.appendChild(countryPopulation);
+        }
+    } catch (e){
+        console.error(e);
+    }
+}
+
+
+
+giveCountry();
