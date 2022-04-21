0974c15105ecc1c04634c5d73a805ff93e214f11
CamielVilla <camielvilla@gmail.com>
2022-04-21 21:59:41 +0200

    begin gemaakt

--- dist/index.d56a3cb1.js
+++ dist/index.d56a3cb1.js
@@ -470,12 +470,39 @@ function giveRegionColor(region) {
     else if (region == "Oceania") return "Oceania";
     else return "Other";
 }
-async function giveCountry() {
+// async function giveCountry() {
+//     try{
+//         const result = await
+//             axios.get('https://restcountries.com/v2/all');
+//             const sorted = result.data.sort((a, b) => a.population - b.population);
+//         for (let i = 0; i < sorted.length ; i++) {
+//             const countryDiv = document.createElement('div')
+//             countryDiv.id = "country-div";
+//             const countryName = document.createElement('p');
+//             countryName.className = giveRegionColor(sorted[i].region);
+//             const countryFlag = document.createElement('img');
+//             const countryPopulation = document.createElement('p');
+//             const countryPopulationNumber = sorted[i].population;
+//             countryName.textContent = sorted[i].name;
+//             countryFlag.src = sorted[i].flag;
+//             countryPopulation.textContent = "Has a population of " + countryPopulationNumber + " people";
+//             const countryList = document.getElementById("country-name");
+//             countryDiv.appendChild(countryName);
+//             countryDiv.appendChild(countryFlag);
+//             countryDiv.appendChild(countryPopulation);
+//             countryList.appendChild(countryDiv);
+//         }
+//     } catch (e){
+//         console.error(e);
+//     }
+// }
+giveCountry();
+async function giveCountry(country) {
     try {
         const result = await _axiosDefault.default.get('https://restcountries.com/v2/all');
         const sorted = result.data.sort((a, b)=>a.population - b.population
         );
-        for(let i = 0; i < sorted.length; i++){
+        for(let i = 0; i < sorted.length; i++)if (country === sorted[i].name) {
             const countryDiv = document.createElement('div');
             countryDiv.id = "country-div";
             const countryName = document.createElement('p');
@@ -496,7 +523,30 @@ async function giveCountry() {
         console.error(e);
     }
 }
-giveCountry();
+// async function searchCountry() {
+//     try {
+//         const result = await
+//         axios.get('https://restcountries.com/v2/all');
+//         const  { data: { name }} = result;
+//         console.log(name);
+//
+//
+//     }catch (e) {
+// console.error(e)
+//     }
+//
+// }
+const button = document.getElementById("button");
+button.addEventListener("click", showCountry);
+const countryInput = document.getElementById("countryname");
+// countryInput.addEventListener("keypress", showInput);
+//
+// function showInput(e) {
+//     console.log(e)
+// }
+function showCountry() {
+    console.log();
+}
 
 },{"axios":"1IeuP","@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}],"1IeuP":[function(require,module,exports) {
 module.exports = require('./lib/axios');
--- src/app.js
+++ src/app.js
@@ -16,31 +16,89 @@ function giveRegionColor(region) {
     }
 
 }
-async function giveCountry() {
+// async function giveCountry() {
+//     try{
+//         const result = await
+//             axios.get('https://restcountries.com/v2/all');
+//             const sorted = result.data.sort((a, b) => a.population - b.population);
+//         for (let i = 0; i < sorted.length ; i++) {
+//             const countryDiv = document.createElement('div')
+//             countryDiv.id = "country-div";
+//             const countryName = document.createElement('p');
+//             countryName.className = giveRegionColor(sorted[i].region);
+//             const countryFlag = document.createElement('img');
+//             const countryPopulation = document.createElement('p');
+//             const countryPopulationNumber = sorted[i].population;
+//             countryName.textContent = sorted[i].name;
+//             countryFlag.src = sorted[i].flag;
+//             countryPopulation.textContent = "Has a population of " + countryPopulationNumber + " people";
+//             const countryList = document.getElementById("country-name");
+//             countryDiv.appendChild(countryName);
+//             countryDiv.appendChild(countryFlag);
+//             countryDiv.appendChild(countryPopulation);
+//             countryList.appendChild(countryDiv);
+//         }
+//     } catch (e){
+//         console.error(e);
+//     }
+// }
+
+giveCountry();
+
+
+async function giveCountry(country) {
     try{
         const result = await
             axios.get('https://restcountries.com/v2/all');
-            const sorted = result.data.sort((a, b) => a.population - b.population);
+        const sorted = result.data.sort((a, b) => a.population - b.population);
+
         for (let i = 0; i < sorted.length ; i++) {
-            const countryDiv = document.createElement('div')
-            countryDiv.id = "country-div";
-            const countryName = document.createElement('p');
-            countryName.className = giveRegionColor(sorted[i].region);
-            const countryFlag = document.createElement('img');
-            const countryPopulation = document.createElement('p');
-            const countryPopulationNumber = sorted[i].population;
-            countryName.textContent = sorted[i].name;
-            countryFlag.src = sorted[i].flag;
-            countryPopulation.textContent = "Has a population of " + countryPopulationNumber + " people";
-            const countryList = document.getElementById("country-name");
-            countryDiv.appendChild(countryName);
-            countryDiv.appendChild(countryFlag);
-            countryDiv.appendChild(countryPopulation);
-            countryList.appendChild(countryDiv);
+            if (country === sorted[i].name) {
+                const countryDiv = document.createElement('div')
+                countryDiv.id = "country-div";
+                const countryName = document.createElement('p');
+                countryName.className = giveRegionColor(sorted[i].region);
+                const countryFlag = document.createElement('img');
+                const countryPopulation = document.createElement('p');
+                const countryPopulationNumber = sorted[i].population;
+                countryName.textContent = sorted[i].name;
+                countryFlag.src = sorted[i].flag;
+                countryPopulation.textContent = "Has a population of " + countryPopulationNumber + " people";
+                const countryList = document.getElementById("country-name");
+                countryDiv.appendChild(countryName);
+                countryDiv.appendChild(countryFlag);
+                countryDiv.appendChild(countryPopulation);
+                countryList.appendChild(countryDiv);
+            }
         }
     } catch (e){
         console.error(e);
     }
 }
+// async function searchCountry() {
+//     try {
+//         const result = await
+//         axios.get('https://restcountries.com/v2/all');
+//         const  { data: { name }} = result;
+//         console.log(name);
+//
+//
+//     }catch (e) {
+// console.error(e)
+//     }
+//
+// }
 
-giveCountry();
+
+const button = document.getElementById("button");
+button.addEventListener("click", showCountry)
+
+const countryInput = document.getElementById("countryname");
+// countryInput.addEventListener("keypress", showInput);
+//
+// function showInput(e) {
+//     console.log(e)
+// }
+function showCountry() {
+    console.log();
+}
